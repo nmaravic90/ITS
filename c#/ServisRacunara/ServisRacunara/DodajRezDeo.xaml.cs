7966f95c9050e1ce90fac3e8d01b2d30673f2648
Nikola Maravic <108002796+nmaravic90@users.noreply.github.com>
2022-06-22 20:38:29 +0200

    Add files via upload

---
+++ c#/ServisRacunara/ServisRacunara/DodajRezDeo.xaml.cs
@@ -0,0 +1,61 @@
+rmal style='margin-left:.5in'><span style='font-size:10.0pt'>iii)
+states that any provisions which differ from this Agreement are offered by that
+Contributor alone and not by any other party; and</span></p>
+
+<p class=MsoNormal style='margin-left:.5in'><span style='font-size:10.0pt'>iv)
+states that source code for the Program is available from such Contributor, and
+informs licensees how to obtain it in a reasonable manner on or through a
+medium customarily used for software exchange.<span style='color:blue'> </span></span></p>
+
+<p><span style='font-size:10.0pt'>When the Program is made available in source
+code form:</span> </p>
+
+<p class=MsoNormal style='margin-left:.5in'><span style='font-size:10.0pt'>a)
+it must be made available under this Agreement; and </span></p>
+
+<p class=MsoNormal style='margin-left:.5in'><span style='font-size:10.0pt'>b) a
+copy of this Agreement must be included with each copy of the Program. </span></p>
+
+<p><span style='font-size:10.0pt'>Contributors may not remove or alter any
+copyright notices contained within the Program. </span></p>
+
+<p><span style='font-size:10.0pt'>Each Contributor must identify itself as the
+originator of its Contribution, if any, in a manner that reasonably allows
+subsequent Recipients to identify the originator of the Contribution. </span></p>
+
+<p><b><span style='font-size:10.0pt'>4. COMMERCIAL DISTRIBUTION</span></b> </p>
+
+<p><span style='font-size:10.0pt'>Commercial distributors of software may
+accept certain responsibilities with respect to end users, business partners
+and the like. While this license is intended to facilitate the commercial use
+of the Program, the Contributor who includes the Program in a commercial
+product offering should do so in a manner which does not create potential
+liability for other Contributors. Therefore, if a Contributor includes the
+Program in a commercial product offering, such Contributor (&quot;Commercial
+Contributor&quot;) hereby agrees to defend and indemnify every other
+Contributor (&quot;Indemnified Contributor&quot;) against any losses, damages and
+costs (collectively &quot;Losses&quot;) arising from claims, lawsuits and other
+legal actions brought by a third party against the Indemnified Contributor to
+the extent caused by the acts or omissions of such Commercial Contributor in
+connection with its distribution of the Program in a commercial product
+offering. The obligations in this section do not apply to any claims or Losses
+relating to any actual or alleged intellectual property infringement. In order
+to qualify, an Indemnified Contributor must: a) promptly notify the Commercial
+Contributor in writing of such claim, and b) allow the Commercial Contributor
+to control, and cooperate with the Commercial Contributor in, the defense and
+any related settlement negotiations. The Indemnified Contributor may participate
+in any such claim at its own expense.</span> </p>
+
+<p><span style='font-size:10.0pt'>For example, a Contributor might include the
+Program in a commercial product offering, Product X. That Contributor is then a
+Commercial Contributor. If that Commercial Contributor then makes performance
+claims, or offers warranties related to Product X, those performance claims and
+warranties are such Commercial Contributor's responsibility alone. Under this
+section, the Commercial Contributor would have to defend claims against the
+other Contributors related to those performance claims and warranties, and if a
+court requires any other Contributor to pay any damages as a result, the
+Commercial Contributor must pay those damages.</span> </p>
+
+<p><b><span style='font-size:10.0pt'>5. NO WARRANTY</span></b> </p>
+
+<p><span style='font-size:10.0pt'>EXCEPT AS EXPRESSLY SET FORTH IN TH
---
+++ c#/ServisRacunara/ServisRacunara/NoviRacun.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ServisRacunara
+{
+    /// <summary>
+    /// Interaction logic for NoviRacun.xaml
+    /// </summary>
+    public partial class NoviRacun : Window
+    {
+        public NoviRacun()
+        {
+            InitializeComponent();
+        }
+
+        private void cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int broj = Convert.ToInt32(cb.SelectedItem);
+
+            ServisRacunaraDataContext DC = new ServisRacunaraDataContext();
+            try
+            {
+                var podaciDC = from x in DC.RadNalogs where x.sifraNaloga == broj select x;
+
+
+                foreach (var podaci in podaciDC)
+                {
+                    tb_imeRadnika.Text = podaci.radnik.ToString();
+                    tb_vreme.Text = podaci.vreme.ToString();
+                    tb_imePrezime.Text = podaci.imePrezime.ToString();
+                    tb_brLicne.Text = podaci.brLicne.ToString();
+                    tb_telefon.Text = podaci.telefon.ToString();
+                    tb_mail.Text = podaci.mail.ToString();
+                    tb_sifRacunara.Text = podaci.sifraRac.ToString();
+                    rtb_opisKvara.AppendText(podaci.opisKva.ToString());
+                    rtb_SpisakPoslova.AppendText(podaci.spisPoslova.ToString());
+                    rtb_SpisakRezDel.AppendText(podaci.rezDel.ToString());
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ServisRacunaraDataContext dt = new ServisRacunaraDataContext();
+            var ID = from x in dt.RadNalogs select x.sifraNaloga;
+            foreach (var sifra in ID)
+            {
+                cb.Items.Add(sifra.ToString());
+            }
+        }
+
+        private void Checked(object sender, RoutedEventArgs e)
+        {
+            int suma = 0;
+            if (cb_dijagnostika.IsChecked == true) suma = suma + 1000;
+            if (cb_popravka.IsChecked == true) suma = suma + 1500;
+            if (cb_deo.IsChecked == true) suma = suma + 500;
+            tb_cenaUsluga.Text = suma.ToString();
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(tb_cenaDelova.Text))
+                tb_cenaDelova.Text = "0";
+            tb_ukupnaCena.Text = (Convert.ToInt32(tb_cenaDelova.Text) + Convert.ToInt32(tb_cenaUsluga.Text)).ToString();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            bool pom = false;
+
+            ServisRacunaraDataContext DC = new ServisRacunaraDataContext();
+
+            try
+            {
+                RacunVlasnika rv = new RacunVlasnika();
+
+                rv.sifraNaloga = Convert.ToInt32(cb.SelectedValue.ToString());
+                rv.radnik = tb_imeRadnika.Text;
+                rv.vreme = tb_vreme.Text;
+
+                rv.imePrezime = tb_imePrezime.Text;
+                rv.brLicne = Convert.ToInt32(tb_brLicne.Text);
+                rv.telefon = tb_telefon.Text;
+                rv.mail = tb_mail.Text;
+                rv.sifraRac = tb_sifRacunara.Text;
+
+                rv.opisKva = new TextRange(rtb_opisKvara.Document.ContentStart, rtb_opisKvara.Document.ContentEnd).Text;
+                rv.spisPoslova = new TextRange(rtb_SpisakPoslova.Document.ContentStart, rtb_SpisakPoslova.Document.ContentEnd).Text;
+                rv.rezDel = new TextRange(rtb_SpisakRezDel.Document.ContentStart, rtb_SpisakRezDel.Document.ContentEnd).Text;
+
+
+                rv.cenaDelova = tb_cenaDelova.Text;
+                rv.cenaUsluga = tb_cenaUsluga.Text;
+                rv.cenaUkupna = tb_ukupnaCena.Text;
+
+                DC.RacunVlasnikas.InsertOnSubmit(rv);
+                DC.SubmitChanges();
+                pom = true;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Doslo je do greskee!");
+            }
+            if (pom)
+            {
+                MessageBox.Show("Uspesno ste kreirali racun!", "Obavestenje", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
+        }
+    }
+}
