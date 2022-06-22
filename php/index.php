76a66b7d4254b2fb79d7b3e34022353e65422d14
Nikola Maravic <108002796+nmaravic90@users.noreply.github.com>
2022-06-22 16:01:13 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,9 +2,6 @@
 <?php 
 include 'php/prijava.php'; 
 include 'php/akcije.php';
-
-
-
 ?>
 <html>
 	<head>
@@ -16,30 +13,21 @@ include 'php/akcije.php';
         <link rel="shortcut icon" href="images/ikonica.ico"><!--DODAVAJNE IKONICE-->
         <link rel="stylesheet" type="text/css"  href="css/style.css">
         <script type="text/javascript" src="js/validacije.js"></script>
-        
-
         <link rel="stylesheet" type="text/css"  href="css/jquery.bxslider.css">
         <script type="text/javascript" src="js/jquery.js" ></script>
         <script type="text/javascript" src="js/jquery.bxslider.js" ></script>
         <script type="text/javascript" src="js/script.js" ></script>
-
-
-
-
 	</head>
 <body>
 	<div id="header">
 		<div class="header-blok1">
 		     <img src="images/logo.png" alt="logo" height="60" wight="120">
-
 		</div>
 		<div class="header-blok2">
-			<div id="meni">
-				<ul>
+			<div id="meni"><ul>
                     <li><a href="index.php" title="">AKCIJE</a></li>
                     <li><a href="racunari.php" title="">RACUNARI</a></li>
                     <li><a href="konfigurator.php" title="">KONFIGURATOR</a></li>
-                    
                 </ul>
 			</div>			
 		</div>
@@ -72,26 +60,17 @@ include 'php/akcije.php';
                         ?>
                         </li>
         </ul>
-    </div>
-
-               
+    </div>  
 	<div id="body">
-
-
                  <div class="slider">
                     <div class="slide"><img src="images/slajd/slajd1.jpg" alt="" /></div>
                     <div class="slide"><img src="images/slajd/slajd2.jpg" alt="" /></div>
                     <div class="slide"><img src="images/slajd/slajd3.jpg" alt="" /></div>
                 </div>
-
-
-
-
 		<div id='body-products'>
             <h1 align="center">AKCIJE</h1>
                 <?php  akcija();?>
-		</div>
-			
+		</div>	
 	</div>
 	<div id="footer">
 		<div id="footer01">
@@ -101,7 +80,6 @@ include 'php/akcije.php';
                         
                             <td>FACEBOOK</td>
                             <td><a href="https://www.facebook.com"><img src="images/fb.png" width="30px" height="30px"/></a></td>
-                        
                         <tr>
                             <td>YOUTUBE</td>
                             <td><a href="https://www.youtube.com"><img src="images/youtube.png" width="30px" height="30px"/></a></td>
@@ -138,7 +116,6 @@ include 'php/akcije.php';
                         </tr>
                     </table>
                   </form>
-				
 			</div>
 			<div class="footer-blok">
 				 <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d181139.85118476648!2d20.4203223!3d44.8152453!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2srs!4v1439660548619" width="293" height="215" frameborder="0" style="border:0">
@@ -146,8 +123,6 @@ include 'php/akcije.php';
 			</div>
 		</div>
 	</div>
-
-
 </body>
 </html>
 
